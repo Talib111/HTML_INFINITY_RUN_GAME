295028cd194075463bb51d469e743f638e209463
Talib111 <talibplay111@gmail.com>
2021-09-15 12:00:33 +0530

    sound added

--- index.js
+++ index.js
@@ -1,24 +1,9 @@
 
 
 var back_scene = document.getElementById('back_scene');
-// let elem = document.getElementById('playg');
-// if (elem.requestFullscreen) {
-//     elem.requestFullscreen();
-//   } else if (elem.webkitRequestFullscreen) { /* Safari */
-//     elem.webkitRequestFullscreen();
-//   } else if (elem.msRequestFullscreen) { /* IE11 */
-//     elem.msRequestFullscreen();
 
-
-// var audioo = document.getElementById('md');
-// audioo.onplay();
-//   }
 var time = 40;
-//time interval
-// var time_interval = setInterval(() => {
-//     time = time - 1;
-//     console.log(time);
-// }, 1000);
+
 var win_widt = parseInt(window.getComputedStyle(back_scene,null).getPropertyValue('width'));
 var div_by_ten = Math.round(win_widt/5)*5;
 console.log(win_widt);
@@ -27,8 +12,7 @@ let interval = setInterval(() => {
     let back_scene_left = parseInt(window.getComputedStyle(back_scene,null).getPropertyValue('left'));
     let next_back = back_scene_left - 5;
     back_scene.style.left=next_back+"px";
-    // console.log(next_back);
-    // console.log(Math.abs(next_back));
+    
     if(Math.abs(next_back) === div_by_ten){
     back_scene.style.left=div_by_ten+"px";
     // console.log(win_widt);
@@ -45,7 +29,16 @@ const stop_int=()=>{
     clearInterval(interval);
 }
 
+// var audio = new Audio('myaud.mp3')
+var audio = new Audio('myaud.mp3')
+var shootSound = new Audio('shoot.wav')
+// shootSound.playbackRate = 2
+audio.playbackRate = 1.5
+
 const jump = (stop_anim)=>{
+    //activate jump sound
+    
+     audio.play()
     document.getElementById('arrow').style.animation="shoot 0.8s linear";
     document.getElementById('runner').style.animation="jmp 0.8s linear";
     setTimeout(stop_anim,800)
@@ -68,6 +61,8 @@ const speed_inc = ()=>{
 }
 
 const fire = ()=>{
+    shootSound.currentTime = 0
+    shootSound.play()
     document.getElementById('shootarrow').style.animation="shoot_me 0.4s linear";
     setTimeout(stop_shoot,400)
 }
